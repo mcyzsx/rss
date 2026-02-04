1de22d169c3c5368039158f3336fef3ad4bc3da9
Mao Chenyu <chenyumao815@outlook.com>
2026-02-04 17:22:07 +0800

    refactor(Gmerss.py): 更新RSS源配置，移除周刊类源并添加个人博客源
    移除不再使用的周刊类RSS源，添加新的个人博客源配置，优化内容获取来源

--- Gmerss.py
+++ Gmerss.py
@@ -10,47 +10,29 @@ displayMax=2 # 每个RSS最多抓取数
 weeklyKeyWord="" # 周刊过滤关键字
 
 rssBase={
-    "独立开发变现":{
-        "url":"https://www.ezindie.com/feed/rss.xml",
-        "type":"weekly",
+    "安知鱼":{
+        "url":"https://blog.anheyu.com/rss.xml",
+        "type":"post",
         "timeFormat":"%a, %d %b %Y %H:%M:%S GMT",
         "nameColor":"#a4244b"
     },
-    "夜枫":{
-        "url":"https://yefengs.com/feed",
+    "张洪heo":{
+        "url":"https://blog.zhheo.com/rss.xml",
         "type":"post",
         "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
         "nameColor":"#b8d101"
     },
-    "kn007":{
-        "url":"https://kn007.net/feed/",
+    "纸鹿":{
+        "url":"https://blog.zhilu.site/atom.xml",
         "type":"post",
         "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
         "nameColor":"#e76976"
     },
-    "二丫讲梵":{
-        "url":"https://wiki.eryajf.net/learning-weekly.xml",
-        "type":"weekly",
+    "APP喵":{
+        "url":"https://www.appmiao.com/feed",
+        "type":"post",
         "timeFormat":"%a, %d %b %Y %H:%M:%S +0800",
         "nameColor":"#93bd76"
-    },
-    "豌豆花下猫":{
-        "url":"https://pythoncat.top/rss.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S GMT",
-        "nameColor":"#bc4c00"
-    },
-    "阮一峰":{
-        "url":"http://www.ruanyifeng.com/blog/atom.xml",
-        "type":"weekly",
-        "timeFormat":"%Y-%m-%dT%H:%M:%SZ",
-        "nameColor":"#1f883d"
-    },
-    "老胡的周刊":{
-        "url":"https://weekly.howie6879.com/rss/rss.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0806",
-        "nameColor":"#A333D0"
     },
     "Meekdai":{
         "url":"https://blog.meekdai.com/rss.xml",
